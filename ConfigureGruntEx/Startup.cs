39b603f1ed5461947289121b37d5c72949f0e0c0
Tam Chieu <tam.chieu@newoceaninfosys.com>
2016-11-26 15:52:34 +0700

    Initial Asp.net mvc Project

---
+++ ConfigureGruntEx/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ConfigureGruntEx.Startup))]
+namespace ConfigureGruntEx
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
